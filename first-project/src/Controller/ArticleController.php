ba2e7ee03a3c7632da905b5b386f082afdb2f616
Galen Senogles <galen.senogles@gmail.com>
2018-10-10 17:46:05 -0700

    Added html twig file, updated article controller to handle comments

--- first-project/src/Controller/ArticleController.php
+++ first-project/src/Controller/ArticleController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 	/**
 	 * @Route("/")
@@ -20,9 +21,14 @@ class ArticleController
 	 */
 	public function show($slug)
 	{
-		return new Response(sprintf(
-			'Future page to show the article: %s',
-			$slug
-		));
+		$comments = [
+			'One time I ate a rock and it did not taste like bacon! Fake news!',
+			'YAAAAS Im going on an all asteroid diet',
+			'I like bacon and I like turtles'
+		];
+		return $this->render('article/show.html.twig', [
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'comments' => $comments,
+		]);
 	}
 }
