1e6eb46a029ad9202cfddf67f256facaf2d0c85e
Galen Senogles <galen.senogles@gmail.com>
2018-10-15 10:54:19 -0700

    Add knpmarkdown bundle, use caching

--- first-project/src/Controller/ArticleController.php
+++ first-project/src/Controller/ArticleController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Michelf\MarkdownInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface ;
 
 class ArticleController extends AbstractController
 {
@@ -21,7 +23,7 @@ class ArticleController extends AbstractController
 	/**
 	 * @Route("/news/{slug}", name="article_show")
 	 */
-	public function show($slug)
+	public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
 	{
 		$comments = [
 			'One time I ate a rock and it did not taste like bacon! Fake news!',
@@ -29,10 +31,45 @@ class ArticleController extends AbstractController
 			'I like bacon and I like turtles'
 		];
 
-		
+		$articleContent = "Spicy jalapeno bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+                                lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+                                labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+                                **turkey** shank eu pork belly meatball non cupim.
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+                                laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+                                capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+                                picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+                                occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+                                mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+                                strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+                                cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+                                fugiat.
+
+Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin buffalo minim ex quis. Pork belly
+                                pork chop officia anim. Irure tempor leberkas kevin adipisicing cupidatat qui buffalo ham aliqua pork belly
+                                exercitation eiusmod. Exercitation incididunt rump laborum, t-bone short ribs buffalo ut shankle pork chop
+                                bresaola shoulder burgdoggen fugiat. Adipisicing nostrud chicken consequat beef ribs, quis filet mignon do.
+                                Prosciutto capicola mollit shankle aliquip do dolore hamburger brisket turducken eu.
+
+Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui beef ribs dolore pariatur.
+                                Minim strip steak fugiat nisi est, meatloaf pig aute. Swine rump turducken nulla sausage. Reprehenderit pork
+                                belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod. Doner drumstick short loin,
+                                adipisicing cow cillum tenderloin.";
+
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        if(!$item->isHit()) {
+        	$item->set($markdown->transform($articleContent));
+        	$cache->save($item);
+        }
+
+        $articleContent = $item->get();
 
 		return $this->render('article/show.html.twig', [
 			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'articleContent' => $articleContent,
 			'slug' => $slug,
 			'comments' => $comments,
 		]);
